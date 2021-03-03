a2f4c6c3c99f6eaab2d97a66003c6f40ccdcd7df
emidyrmishi <sheme@Cogito-iMac-27.local>
2021-03-03 18:12:31 +0100

    typo

--- l10n_it_fatturapa/models/ir_attachment.py
+++ l10n_it_fatturapa/models/ir_attachment.py
@@ -118,10 +118,10 @@ class Attachment(models.Model):
     def get_fattura_elettronica_preview(self):
 
         invoice_attachment_obj = self.env[self.res_model].browse(self.res_id)
-        xsl_path = get_module_resource('l10n_it_fatturapa', 'data', self.env.user.company_id.fatturapa_preview_style)
-
         if invoice_attachment_obj.simplified_invoice:
             xsl_path = get_module_resource('l10n_it_fatturapa', 'data', 'fatturasemplificata_v1.0.xsl')
+        else:
+            xsl_path = get_module_resource('l10n_it_fatturapa', 'data', self.env.user.company_id.fatturapa_preview_style)
 
         xslt = ET.parse(xsl_path)
         xml_string = self.get_xml_string()
