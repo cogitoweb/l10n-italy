5701018e4c118dbfea39708fe3f1cd89101be8cb
Riccardo Pittalis <riccardo.pittalis@cartesia.dev>
2025-02-05 13:01:00 +0100

    update query, fix kanban problem

--- l10n_it_fatturapa_pec/virtual/fatturapa_attachment_out_state.py
+++ l10n_it_fatturapa_pec/virtual/fatturapa_attachment_out_state.py
@@ -13,7 +13,6 @@ class VirtualFatturaPAAttachmentOutState(models.Model):
     state = fields.Char(readonly=True)
     total_fatturapa_out = fields.Integer(readonly=True)
 
-
     # This is executed on every module update
     @api.model_cr
     def init(self):
@@ -22,16 +21,34 @@ class VirtualFatturaPAAttachmentOutState(models.Model):
 
         _sql_view = """
             CREATE OR REPLACE VIEW %(table)s AS
+            WITH states AS (
+                SELECT 'ready' AS state
+                UNION
+                SELECT 'sent'
+                UNION
+                SELECT 'sender_error'
+                UNION
+                SELECT 'recipient_error'
+                UNION
+                SELECT 'rejected'
+                UNION
+                SELECT 'validated'
+                UNION
+                SELECT 'accepted'
+            )
             SELECT
-                ROW_NUMBER() OVER() AS id,
-                state,
-                count(*) as total_fatturapa_out
+                row_number() over() AS id,
+                s.state,
+                coalesce(count(fao.state), 0) AS total_fatturapa_out
             FROM
-                fatturapa_attachment_out fao
-            WHERE
-                create_date >= date_trunc('month', now()) AND create_date < date_trunc('month', now()) + '1 month'::interval
+                states s
+            LEFT JOIN fatturapa_attachment_out fao ON fao.state = s.state
+            AND
+                fao.create_date >= date_trunc('month', now()) AND fao.create_date < date_trunc('month', now()) + '1 month'::interval
             GROUP BY
-                state
+                s.state
+            ORDER BY
+                s.state
         """
 
         self.env.cr.execute(_sql_view, {'table': AsIs(view_name)})
