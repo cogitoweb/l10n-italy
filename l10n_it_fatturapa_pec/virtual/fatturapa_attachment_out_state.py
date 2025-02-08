5af3f1e358a1f215fbd328a419c2a3f75b77221b
Riccardo Pittalis <riccardo.pittalis@cartesia.dev>
2025-02-09 00:03:50 +0100

    improved kanban, change state to selection to manage translations, add filters (to be fixed)

--- l10n_it_fatturapa_pec/virtual/fatturapa_attachment_out_state.py
+++ l10n_it_fatturapa_pec/virtual/fatturapa_attachment_out_state.py
@@ -10,9 +10,39 @@ class VirtualFatturaPAAttachmentOutState(models.Model):
     _auto = False
 
     # fields
-    state = fields.Char(readonly=True)
+    state = fields.Selection(
+        [
+            ('ready', 'Ready to Send'),
+            ('sent', 'Sent'),
+            ('sender_error', 'Sender Error'),
+            ('recipient_error', 'Not delivered'),
+            ('rejected', 'Rejected (PA)'),
+            ('validated', 'Delivered'),
+            ('accepted', 'Accepted'),
+        ],
+        string='State',
+        readonly=True
+    )
     total_fatturapa_out = fields.Integer(readonly=True)
     total_fatturapa_out_actual_month = fields.Integer(readonly=True)
+
+    @api.multi
+    def _get_action(self, action_xmlid):
+        action = self.env.ref(action_xmlid).read()[0]
+        if self:
+            action['display_name'] = self.display_name
+        return action
+
+    # filter methods
+    @api.multi
+    def get_action_fatturapa_current_month_tree(self):
+        """ extra action to filter on dashboard """
+        return self._get_action('l10n_it_fatturapa_pec.action_fatturapa_current_month_tree')
+
+    @api.multi
+    def get_action_fatturapa_current_year_tree(self):
+        """ extra action to filter on dashboard """
+        return self._get_action('l10n_it_fatturapa_pec.action_fatturapa_current_year_tree')
 
     # This is executed on every module update
     @api.model_cr
